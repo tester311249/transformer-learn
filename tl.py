ce4dddbdb7b573347b6e73acad569249dccd3c2a
tester311249 <samir_pratihari@yahoo.co.in>
2026-01-14 13:42:24 +0000

    First Commit

---
+++ tl.py
@@ -0,0 +1,5 @@
+from datasets import list_datasets
+
+all_datasets = list_datasets()
+print(f"There are {len(all_datasets)} datasets currently available on the Hub")
+print(f"The first 10 are: {all_datasets[:10]}")
